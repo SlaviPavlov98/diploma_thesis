0dae9501853744dc712fcdb6800985406c8cab06
Slavi Pavlov <49622152+SlaviPavlov98@users.noreply.github.com>
2024-10-03 11:31:31 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -16,15 +16,12 @@ def get_area(a, b, c):
         return "These sides do not form a valid triangle"
 
 
-def is_valid_triangle(a, b, c):
-    return a + b > c and a + c > b and b + c > a
-
 
 
 # Example usage
 a, b, c = 3, 4, 5
 
-print(f'Is valid triangle: {is_valid_triangle(a, b, c)}')
+# print(f'Is valid triangle: {is_valid_triangle(a, b, c)}')
 print(f"Perimeter: {get_perimeter(a, b, c)}")
 print(f"Area: {get_area(a, b, c)}")
 # print(f"Angles: {get_angles(a, b, c)}")
