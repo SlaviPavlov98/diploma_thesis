e0e8d1923b2c7360b24cd44f1f3b6c15895ec34e
Slavi Pavlov <49622152+SlaviPavlov98@users.noreply.github.com>
2025-01-04 17:32:27 +0200

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -16,10 +16,17 @@ def get_area(a, b, c):
         return "These sides do not form a valid triangle"
 
 
+def get_angles(a, b, c):
+    A = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+    B = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+    C = 180 - A - B
+    return A, B, C
+
+
 # Example usage
 a, b, c = 3, 4, 5
 
 # print(f'Is valid triangle: {is_valid_triangle(a, b, c)}')
 print(f"Perimeter: {get_perimeter(a, b, c)}")
 print(f"Area: {get_area(a, b, c)}")
-# print(f"Angles: {get_angles(a, b, c)}")
+print(f"Angles: {get_angles(a, b, c)}")
